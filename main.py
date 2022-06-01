91487724a4b1a9810a38578b9589f16b44623bf4
cu8306ba <104530369+cu8306ba@users.noreply.github.com>
2022-05-31 22:02:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'Unk980929'
+        tl_admin_user = 'Abdielsn'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
